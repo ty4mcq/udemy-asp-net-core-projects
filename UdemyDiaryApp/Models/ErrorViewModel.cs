999313880b8a49a8e3d6d62727382455e3c3a5da
ty4mcq <ty4mcq.2101@hotmail.com>
2024-11-01 15:54:59 +0000

    Add project files.

---
+++ UdemyDiaryApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UdemyDiaryApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
