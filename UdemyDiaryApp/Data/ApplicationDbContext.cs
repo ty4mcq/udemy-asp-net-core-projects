f3b248d4b6d389568599f13753927a9dd5a0cce9
ty4mcq <ty4mcq.2101@hotmail.com>
2024-11-07 12:30:17 +0000

    Added ApplicationDbContext.cs

---
+++ UdemyDiaryApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UdemyDiaryApp.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
