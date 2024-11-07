94c0febfc4116364a8cbad3596d2899afa3392ed
ty4mcq <ty4mcq.2101@hotmail.com>
2024-11-07 12:19:21 +0000

    Create DiaryEntry.cs Model

---
+++ UdemyDiaryApp/Models/DiaryEntry.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UdemyDiaryApp.Models;
+
+public class DiaryEntry
+{
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; } = string.Empty;
+    [Required]
+    public string Content { get; set; } = string.Empty;
+    [Required]
+    public DateTime Created { get; set; } = DateTime.Now;
+}
