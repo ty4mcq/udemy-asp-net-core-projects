e08c516884a2ce3de0baf661bf1cf64d965f7bf9
ty4mcq <ty4mcq.2101@hotmail.com>
2024-11-07 12:53:05 +0000

    Added DiaryEntriesController.cs + Seeding Data

---
+++ UdemyDiaryApp/Controllers/DiaryEntriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UdemyDiaryApp.Controllers;
+
+public class DiaryEntriesController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- UdemyDiaryApp/Data/ApplicationDbContext.cs
+++ UdemyDiaryApp/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using UdemyDiaryApp.Models;
 
 namespace UdemyDiaryApp.Data;
 
@@ -6,5 +7,26 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+    }
+    
+    public DbSet<DiaryEntry> DiaryEntries { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<DiaryEntry>().HasData(
+            new DiaryEntry {
+                Id = 1,
+                Title = "First Entry",
+                Content = "This is the first entry in the diary.",
+                Created = DateTime.Now
+            },
+            new DiaryEntry {
+                Id = 2,
+                Title = "Second Entry",
+                Content = "This is the second entry in the diary.",
+                Created = DateTime.Now
+            }
+            );
     }
 }
