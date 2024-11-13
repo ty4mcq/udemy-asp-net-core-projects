37a1854df68eee978201fbc05b1d019f97f65270
ty4mcq <ty4mcq.2101@hotmail.com>
2024-11-13 18:44:16 +0000

    Added POST action to add new entries

--- UdemyDiaryAPI/Controllers/DiaryEntriesController.cs
+++ UdemyDiaryAPI/Controllers/DiaryEntriesController.cs
@@ -34,5 +34,15 @@ namespace UdemyDiaryAPI.Controllers
             }
             return diaryEntry;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<DiaryEntry>> PostDiaryEntry(DiaryEntry diaryEntry)
+        {
+            diaryEntry.Id = 0;
+            _context.DiaryEntries.Add(diaryEntry);
+            await _context.SaveChangesAsync();
+            var resourceUrl = Url.Action(nameof(GetDiaryEntry), new { id = diaryEntry.Id });
+            return Created(resourceUrl, diaryEntry);
+        }
     }
 }
