1190b7d94fd82237c49cd27032a6fbc9b6333908
ty4mcq <ty4mcq.2101@hotmail.com>
2024-11-13 11:59:52 +0000

    Added Edit + Delete Functions

--- UdemyDiaryApp/Controllers/DiaryEntriesController.cs
+++ UdemyDiaryApp/Controllers/DiaryEntriesController.cs
@@ -23,7 +23,6 @@ public class DiaryEntriesController : Controller
     {
         return View();
     }
-    
     [HttpPost]
     public IActionResult Create(DiaryEntry obj)
     {
@@ -39,5 +38,62 @@ public class DiaryEntriesController : Controller
             return RedirectToAction("Index");
         }
         return View(obj);
+    }
+    
+    [HttpGet]
+    public IActionResult Edit(int? id)
+    {
+        if(id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var diaryEntry = _db.DiaryEntries.Find(id);
+        
+        if(diaryEntry == null)
+        {
+            return NotFound();
+        }
+        
+        return View(diaryEntry);
+    }
+    [HttpPost]
+    public IActionResult Edit(DiaryEntry obj)
+    {
+        if(obj != null && obj.Title.Length < 3)
+        {
+            ModelState.AddModelError("Title", "Title is too short.");
+        }
+        
+        if (ModelState.IsValid)
+        {
+            _db.DiaryEntries.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+    
+    [HttpGet]
+    public IActionResult Delete(int? id)
+    {
+        if(id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var diaryEntry = _db.DiaryEntries.Find(id);
+        
+        if(diaryEntry == null)
+        {
+            return NotFound();
+        }
+        
+        return View(diaryEntry);
+    }
+    [HttpPost]
+    public IActionResult Delete(DiaryEntry obj)
+    {
+        _db.DiaryEntries.Remove(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
     }
 }
---
+++ UdemyDiaryApp/Views/DiaryEntries/Edit.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UdemyDiaryApp.Views.DiaryEntries;
+
+public class Edit : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
